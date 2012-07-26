562b1c43e063fd59c90c70bf208e8a5a30143729
Markus Nolvi <markus.nolvi@gmail.com>
2012-07-27 01:00:19 +0300

    Fixed some typos

--- src/traymemo.pro
+++ src/traymemo.pro
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with TrayMemo.  If not, see <http://www.gnu.org/licenses/>.
 
+
 QT       += gui svg
 
 CONFIG  += qxt
--- src/traymemowindow.h
+++ src/traymemowindow.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 # Copyright (C) 2011 Markus Nolvi
 #
-# This file is part of traymemo.
+# This file is part of TrayMemo.
 # For more information, see <http://code.google.com/p/traymemo/>
 #
 # traymemo is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with traymemo.  If not, see <http://www.gnu.org/licenses/>.
+# along with TrayMemo.  If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************/
 
 #ifndef TRAYMEMOWINDOW_H
