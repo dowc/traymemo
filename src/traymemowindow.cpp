ef0c23c9ffc8c5bf709b6e33121fb7ce584adbe6
Markus Nolvi <markus.nolvi@gmail.com>
2012-08-23 00:36:50 +0300

    Updated version number + nsi script

--- src/traymemowindow.cpp
+++ src/traymemowindow.cpp
@@ -421,14 +421,14 @@ void TrayMemoWindow::showAboutMessage()
 //    QMessageBox *about = new QMessageBox(this);
 //    about->setWindowTitle(tr("About Traymemo"));
 //    about->setText(tr("<b>TrayMemo</b><br>"
-//                      "Version 0.76<br>"
+//                      "Version 0.77<br>"
 //                      "Author: Markus Nolvi<br>"
 //                      "E-mail: markus.nolvi@gmail.com"));
 //    about->setDefaultButton(QMessageBox::Ok);
 //    about->exec();
     QMessageBox::about(this, tr("About Traymemo"),
                              tr("<b>TrayMemo</b><br>"
-                                "Version 0.76<br>"
+                                "Version 0.77<br>"
                                 "Author: Markus Nolvi<br>"
                                 "E-mail: markus.nolvi@gmail.com"));
 }
